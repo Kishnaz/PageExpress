3028bccb41788fc489cbcb1e02b915278626e47d
Kishnaz Shabbir <60220035+Kishnaz@users.noreply.github.com>
2020-02-28 12:06:18 +0530

    Added module

--- modules/documentDB/outputs.tf
+++ modules/documentDB/outputs.tf
@@ -2,6 +2,7 @@ output "dbmaster_arn" {
     value = aws_docdb_cluster_instance.cluster_instances.arn
     }
 
+
 output "dbmaster_address" { 
     value = aws_docdb_cluster_instance.cluster_instances.endpoint
     }
