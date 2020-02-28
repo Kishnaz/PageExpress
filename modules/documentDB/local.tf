7c47626de8f2b8910baa3f1bc624ce2a1692f867
Kishnaz Shabbir <60220035+Kishnaz@users.noreply.github.com>
2020-02-28 12:04:19 +0530

    Update local.tf
    Added module

--- modules/documentDB/local.tf
+++ modules/documentDB/local.tf
@@ -2,7 +2,6 @@ locals {
   tags = {
     "tr:resource-owner"               = var.ResourceOwner
     "tr:application-asset-insight-id" = var.ApplicationAssetInsightId
-    "tr:environment-type"             = var.EnvironmentType
-    "tr:financial-identifier"         = var.FinancialIdentifier
+    "tr:environment-type"             = var.Environment
   }
 }
