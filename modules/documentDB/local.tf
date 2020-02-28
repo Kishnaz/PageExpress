5f4a6cc27e1206e92a73500d72baabf5c8bff4ab
Kishnaz Shabbir <60220035+Kishnaz@users.noreply.github.com>
2020-02-28 12:05:36 +0530

    Added module

--- modules/documentDB/local.tf
+++ modules/documentDB/local.tf
@@ -1,7 +1,9 @@
 locals {
+  
   tags = {
     "tr:resource-owner"               = var.ResourceOwner
     "tr:application-asset-insight-id" = var.ApplicationAssetInsightId
     "tr:environment-type"             = var.Environment
   }
+  
 }
